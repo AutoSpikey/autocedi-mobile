23d790f5a85b81eb0c4ce349865fa1f9892f18df
obengkofijames <obengkofijames@gmail.com>
2023-11-29 14:02:13 +0000

    Home page cleanup

--- screens/Home.js
+++ screens/Home.js
@@ -1,87 +1,63 @@
-import { View, Text, SafeAreaView, StyleSheet, Image, ImageBackground, ScrollView, Dimensions } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import React from 'react';
+import { View, Text, ScrollView, StyleSheet, Image, Dimensions } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
 
 const Home = () => {
-  const navigation = useNavigation()
+  const navigation = useNavigation();
   const windowHeight = Dimensions.get('window').height;
 
   const handleCreateAutomationPress = () => {
-    navigation.navigate('Automation')
-  }
+    navigation.navigate('Automation');
+  };
 
   const automationData = [
-    { id: 1, label: 'Light Bill', description: ' These examples are programmatically compiled from various online out these examples.' },
-    { id: 2, label: 'School', description: 'These examples are programmatically compiled from various online out these examples.' },
-    { id: 3, label: 'Payment', description: 'These examples are programmatically compiled from various online out these examples.' },
-  ]
+    { id: 1, label: 'Light Bill', description: 'These examples are programmatically compiled from various online out these examples.', link: 'LightBillScreen' },
+    { id: 2, label: 'School', description: 'These examples are programmatically compiled from various online out these examples.', link: 'SchoolScreen' },
+    { id: 3, label: 'Payment', description: 'These examples are programmatically compiled from various online out these examples.', link: 'PaymentScreen' },
+    { id: 4, label: 'Payment', description: 'These examples are programmatically compiled from various online out these examples.', link: 'PaymentScreen' },
+  ];
+
+  const handleAutomationPress = (link) => {
+    navigation.navigate(link);
+  };
 
   return (
-
     <ScrollView>
-
       <View style={styles.container}>
-
-        <View style={[styles.top,]}>
-          <TouchableOpacity
-            onPress={handleCreateAutomationPress}
-          >
-            <Text style={styles.topText} >Create Automations</Text>
+        <View style={styles.top}>
+          <TouchableOpacity onPress={handleCreateAutomationPress}>
+            <Text style={styles.topText}>Create Automations</Text>
           </TouchableOpacity>
         </View>
       </View>
 
-      <ScrollView style={{ height: windowHeight, backgroundColor: "white", marginTop: 30 }}>
+      <ScrollView style={[styles.scrollView,{height:windowHeight}]}>
         <View style={styles.bottomHeader}>
           <View>
             <Text style={styles.bottomHeaderText}>Recent Automations</Text>
           </View>
         </View>
 
-        {
-          automationData.map(automationData => (
-            <TouchableOpacity key={automationData.id} style={styles.bottomContainer}>
-              <View style={styles.bottomListContainer}>
-
-                <View>
-                  <Text>{automationData.label}</Text>
-                  <Image source={require('../assets/auto.png')} style={{ width: 30, height: 30, marginTop:10, }} />
-                </View>
-
-                <View style={styles.descriptionContainer}>
-                  <Text style={styles.descriptionHeader}>Description</Text>
-                  <Text style={styles.descriptionText}>{automationData.description}</Text>
-                </View>
-
+        {automationData.map(automationItem => (
+          <TouchableOpacity key={automationItem.id} style={styles.bottomContainer} onPress={() => handleAutomationPress(automationItem.link)}>
+            <View style={styles.bottomListContainer}>
+              <View>
+                <Text>{automationItem.label}</Text>
+                <Image source={require('../assets/auto.png')} style={styles.image} />
               </View>
-            </TouchableOpacity>
-          ))}
 
-
-
-        <TouchableOpacity style={styles.bottomContainer}>
-          <View style={styles.bottomListContainer}>
-
-            <View>
-              <Text>Payment</Text>
-              <Image source={require('../assets/auto.png')} style={{ width: 40, height: 40 }} />
+              <View style={styles.descriptionContainer}>
+                <Text style={styles.descriptionHeader}>Description</Text>
+                <Text style={styles.descriptionText}>{automationItem.description}</Text>
+              </View>
             </View>
-
-            <View style={styles.descriptionContainer}>
-              <Text style={styles.descriptionHeader}>Description</Text>
-              <Text style={styles.descriptionText}>These examples are programmatically compiled from various online out these examples.</Text>
-            </View>
-
-          </View>
-        </TouchableOpacity>
-
+          </TouchableOpacity>
+        ))}
       </ScrollView>
-
-
     </ScrollView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -100,11 +76,10 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: "500"
   },
-  bottomContainer: {
-    backgroundColor: "#dfeef7",
-    margin: 10,
-    borderRadius: 10,
-    padding: 5,
+  scrollView: {
+    // height: windowHeight,
+    backgroundColor: "white",
+    marginTop: 30,
   },
   bottomHeader: {
     display: "flex",
@@ -116,10 +91,25 @@ const styles = StyleSheet.create({
   bottomHeaderText: {
     color: "grey"
   },
+  bottomContainer: {
+    backgroundColor: "#dfeef7",
+    margin: 10,
+    borderRadius: 10,
+    padding: 5,
+  },
   bottomListContainer: {
     padding: 10,
     flexDirection: "row",
     justifyContent: "space-between"
+  },
+  image: {
+    width: 30,
+    height: 30,
+    marginTop: 10,
+  },
+  largeImage: {
+    width: 40,
+    height: 40,
   },
   descriptionHeader: {
     display: "flex",
@@ -132,8 +122,8 @@ const styles = StyleSheet.create({
   },
   descriptionText: {
     color: "grey",
-    // width:300
+    marginTop: 10,
   }
 });
 
-export default Home
+export default Home;
